23620a4d78f3afba8b6c2f58dfb7db956d98a323
Andrey Sokolov <keremet@solaris.kirov.ru>
2017-01-25 20:57:44 +0300

    Переход на список участников после добавления

--- php/uch_save.php
+++ php/uch_save.php
@@ -1,17 +1,19 @@
-﻿<html>
-<head>
-	<meta charset="utf-8">
-</head>
-<body>
-<?php
+﻿<?php
 	include 'connect.php';
 	$stmt = $db->prepare("INSERT INTO ofv_uch (name, type_id, birthday, address) VALUES (?, ?, ?, ?)");
 	if(!$stmt->execute(array( $_POST['ofv_uch_name'],1,'1985-05-06','ТРУЪЪЪЪЪЪЪЪЪ')))
 	{
-		echo 'Ошибка'; print_r($stmt->errorInfo()); echo '<br>';
+?> 
+		<html>
+		<head>
+			<meta charset="utf-8">
+		</head>
+		<body>
+		Ошибка 	<?php print_r($stmt->errorInfo()); 	?> 
+		</body>
+		</html>		
+<?php
 	}
 	else 
-		echo 'Ништяк!';
+		header( 'Location: index.php' );
 ?> 
-</body>
-</html>
