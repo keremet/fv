675dc97db33ec1c5ea69c147be4895c5351e918b
Andrey Sokolov <keremet@solaris.kirov.ru>
2017-01-27 22:29:47 +0300

    Реализовано изменение участника

--- php/uch.php
+++ php/uch.php
@@ -57,7 +57,7 @@ function saveUch()
 include 'connect.php';
 if($id != null){
 	$stmt = $db->prepare(
-		"SELECT * 
+		"SELECT id, name, name as type_name, address, DATE_FORMAT(birthday, '%d%m%Y') as birthday, LPAD(pasp_ser, 4, '0') as pasp_ser, LPAD(pasp_num, 6, '0') as pasp_num, DATE_FORMAT(pasp_date, '%d%m%Y') as pasp_date, pasp_who
 		 FROM ofv_uch
 		 WHERE id = ?");
 	$stmt->execute(array($id));
@@ -67,14 +67,15 @@ if($id != null){
 
 <form id="main_form" action="uch_save.php" method="post">
 <table border="0" cellpadding="0" cellspacing="2">
-<tr><td>ФИО/Название<td><input id="ofv_uch_name"  name="ofv_uch_name" size="30" type="text" value="<?=(($id!=null)?$uch['name']:'')?>">
-<tr><td>Адрес<td><input id="ofv_uch_address"  name="ofv_uch_address" size="30" type="text">
-<tr><td>Дата рождения<td><input id="ofv_uch_birthday"  name="ofv_uch_birthday" size="8" type="text" maxlength="8" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
-<tr><td>Серия паспорта<td><input id="ofv_uch_pasp_ser"  name="ofv_uch_pasp_ser" size="4" type="text" maxlength="4" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
-<tr><td>Номер паспорта<td><input id="ofv_uch_pasp_num"  name="ofv_uch_pasp_num" size="6"  maxlength="6" type="text" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
-<tr><td>Кем выдан паспорт<td><input id="ofv_uch_pasp_who"  name="ofv_uch_pasp_who" size="30" type="text">
-<tr><td>Дата выдачи паспорта<td><input id="ofv_uch_pasp_date"  name="ofv_uch_pasp_date" size="8"  maxlength="8" type="text" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
-<tr><td>Тип участника<td><select name="ofv_uch_type" id="ofv_uch_type">'
+<tr><td>ФИО/Название<td><input id="ofv_uch_name"  name="ofv_uch_name" size="30" type="text" value="<?=(($uch)?$uch['name']:'')?>">
+<tr><td>Адрес<td><input id="ofv_uch_address"  name="ofv_uch_address" size="30" type="text" value="<?=(($uch)?$uch['address']:'')?>">
+<tr><td>Дата рождения<td><input id="ofv_uch_birthday"  name="ofv_uch_birthday" size="8" type="text" value="<?=(($uch)?$uch['birthday']:'')?>" maxlength="8" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
+<tr><td>Серия паспорта<td><input id="ofv_uch_pasp_ser"  name="ofv_uch_pasp_ser" size="4" type="text" value="<?=(($uch)?$uch['pasp_ser']:'')?>" maxlength="4" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
+<tr><td>Номер паспорта<td><input id="ofv_uch_pasp_num"  name="ofv_uch_pasp_num" size="6"  maxlength="6" type="text" value="<?=(($uch)?$uch['pasp_num']:'')?>" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
+<tr><td>Кем выдан паспорт<td><input id="ofv_uch_pasp_who"  name="ofv_uch_pasp_who" size="30" type="text" value="<?=(($uch)?$uch['pasp_who']:'')?>">
+<tr><td>Дата выдачи паспорта<td><input id="ofv_uch_pasp_date"  name="ofv_uch_pasp_date" size="8"  maxlength="8" type="text" value="<?=(($uch)?$uch['pasp_date']:'')?>" onkeyup="return proverka_dat(this);" onchange="return proverka_dat(this);">
+<tr><td>Тип участника<td><select name="ofv_uch_type" id="ofv_uch_type">
+     
 <?php
 	foreach($db->query(
 		"SELECT id, name
@@ -84,6 +85,7 @@ if($id != null){
 		echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
 	}
 ?>
+            
 </select>
 </table>
 
--- php/uch_save.php
+++ php/uch_save.php
@@ -1,7 +1,6 @@
 <?php
 	include 'connect.php';
-	function execStmt($qry, $arr)
-	{
+	function execStmt($qry, $arr) {
 		global $db;
 		$stmt = $db->prepare($qry);
 		if (!$stmt->execute($arr)) {
@@ -15,16 +14,25 @@
 			</body>
 			</html>		
 <?php
-    } else {
-        header('Location: index.php');
-    }
-}
+		} else {
+			header('Location: index.php');
+		}
+	}
 	
-	if($_POST['id'] != ''){
-		execStmt("UPDATE ofv_uch SET name = ? WHERE id = ?",
-			array( $_POST['ofv_uch_name'],$_POST['id']));
-	}else{
-		execStmt("INSERT INTO ofv_uch (name, type_id, birthday, address, pasp_ser, pasp_num, pasp_date, pasp_who) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-			array( $_POST['ofv_uch_name'],$_POST['ofv_uch_type'],$_POST['ofv_uch_birthday_cor'],$_POST['ofv_uch_address'],$_POST['ofv_uch_pasp_ser'],$_POST['ofv_uch_pasp_num'],$_POST['ofv_uch_pasp_date_cor'],$_POST['ofv_uch_pasp_who']));
+	if ($_POST['id'] != '') {
+		execStmt("UPDATE ofv_uch SET name = ?, address = ?, birthday = ?
+					, pasp_ser = ?, pasp_num = ?, pasp_date = ?, pasp_who = ? 
+				  WHERE id = ?",
+			array($_POST['ofv_uch_name'],$_POST['ofv_uch_address'],$_POST['ofv_uch_birthday_cor']
+					,$_POST['ofv_uch_pasp_ser'],$_POST['ofv_uch_pasp_num'],$_POST['ofv_uch_pasp_date_cor'],$_POST['ofv_uch_pasp_who']
+					,$_POST['id']));
+	} else {
+		execStmt("INSERT INTO ofv_uch (name, type_id, birthday
+					, address, pasp_ser
+					, pasp_num, pasp_date, pasp_who) 
+				  VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+			array( $_POST['ofv_uch_name'],$_POST['ofv_uch_type'],$_POST['ofv_uch_birthday_cor']
+				  ,$_POST['ofv_uch_address'],$_POST['ofv_uch_pasp_ser']
+				  ,$_POST['ofv_uch_pasp_num'],$_POST['ofv_uch_pasp_date_cor'],$_POST['ofv_uch_pasp_who']));
 	}
 ?>
