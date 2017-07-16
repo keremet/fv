d29f8496b7a0ce4a2e64507f09fd5a1fd5820212
Andrey Sokolov <keremet@solaris.kirov.ru>
2017-07-16 11:10:39 +0300

    Убраны паспортные данные из списка участников

--- php/uch_list.php
+++ php/uch_list.php
@@ -13,19 +13,14 @@
 	include "oft_table.php";
 	include "connect.php";
 	oftTable::init('Участники');
-	oftTable::header(array('ФИО','АДРЕС'
-			,'ДАТА РОЖДЕНИЯ','ПАСПОРТ: СЕРИЯ','НОМЕР','ДАТА ВЫДАЧИ'
-			,'КЕМ ВЫДАН', ''));
+	oftTable::header(array('ФИО','АДРЕС','ДАТА РОЖДЕНИЯ', ''));
 	foreach($db->query(
 			"SELECT id, name, address, to_char(birthday, 'dd-mm-yyyy') as birthday
-				, pasp_ser, pasp_num
-				, to_char(pasp_date, 'dd-mm-yyyy') as pasp_date, pasp_who 
 			 FROM uch
 			 ORDER BY name"
 			) as $row){
 	oftTable::row(array('<a href=uch.php?id='.$row['id'].'>'.$row['name'].'</a>',$row['address']
-		,$row['birthday'],$row['pasp_ser'],$row['pasp_num'],$row['pasp_date']
-		,$row['pasp_who'], '<a href=acc_list.php?uch_id='.$row['id'].'>Счета</a>'));
+		,$row['birthday'], '<a href=acc_list.php?uch_id='.$row['id'].'>Счета</a>'));
 	}
 
 	oftTable::end();
